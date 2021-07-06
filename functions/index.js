88269ed7071790e8cc001e2e4367acf24207f26c
Suresh  Sangra <suresh.sangra@calsoftinc.com>
2021-07-07 02:09:31 +0530

    automotive-changes

---
+++ functions/index.js
@@ -0,0 +1,43 @@
+const functions = require("firebase-functions");
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+
+const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
+const express = require('express');
+const app = express();
+app.use(express.static('.'));
+
+const YOUR_DOMAIN = 'http://localhost:3000/checkout';
+
+app.post('/create-checkout-session', async (req, res) => {
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ['card'],
+    line_items: [
+      {
+        price_data: {
+          currency: 'usd',
+          product_data: {
+            name: 'Stubborn Attachments',
+            images: ['https://i.imgur.com/EHyR2nP.png'],
+          },
+          unit_amount: 2000,
+        },
+        quantity: 1,
+      },
+    ],
+    mode: 'payment',
+    success_url: `${YOUR_DOMAIN}?success=true`,
+    cancel_url: `${YOUR_DOMAIN}?canceled=true`,
+  });
+
+  res.redirect(303, session.url)
+});
+
+app.listen(4242, () => console.log('Running on port 4242'));
+ 
---
+++ src/components/Navbar.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="hello">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <router-link class="navbar-brand" to="/">Automotive Parts</router-link>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <router-link class="nav-link" to="/home">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/about">About</router-link>
+          </li>
+        </ul>
+      </div>
+      <form class="form-inline my-2 my-lg-0">
+         <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <a
+          class="btn btn-outline-success my-2 my-sm-0"
+          data-toggle="modal"
+          data-target="#login"
+          >Login</a
+        >
+        <a
+          class="btn btn-outline-info border-0 mx-2 my-2 my-sm-0"
+          data-toggle="modal"
+          data-target="#miniCart"
+        >
+          <i class="fas fa-cart-plus"></i>
+        </a>
+      </form>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  props: {
+    msg: String,
+  },
+  components: {},
+
+  methods: {
+     editModal() {
+    	$('#login').modal('show');
+    },
+  },
+
+};
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+@media (min-width: 992px) {
+  .navbar.custom-nav {
+    padding-top: 16px;
+    padding-bottom: 16px;
+    background-color: #fff !important;
+  }
+}
+</style>
